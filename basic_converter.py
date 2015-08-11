0d196fddea909dcec41db37befb5298a00bff477
jonathanmonreal <jem177@case.edu>
2015-08-11 13:01:52 -0400

    add dec_to_bin

--- basic_converter.py
+++ basic_converter.py
@@ -15,3 +15,20 @@ def bin_to_dec(n):
         d += int(b) * (2 ** (digits - i))
     print s[3:]
     return d
+
+def dec_to_bin(n, num_bytes = 0):
+    n = int(n)
+    b = []
+    pad1 = len(str(n))
+    pad2 = len(str(n / 2))
+
+    while(n > 0):
+        print '%*d / 2 = %*dR%d' % (pad1, n, pad2, n // 2, n % 2)
+        b.insert(0, str(n % 2))
+        n //= 2
+
+    if num_bytes * 4 > len(b):
+        for i in range(0, num_bytes * 4 - len(b)):
+            b.insert(0, '0')
+    
+    return ''.join(b)
